c3d74231c5766520ef09d61af16fb94d65eb2a08
Mary1525 <mariefrey25@gmail.com>
2021-04-09 13:35:41 +0200

    Puzzle Labyrinthe

---
+++ Assets/Script/Player/CameraController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float m_mouseSensitivity = 100;
+    [SerializeField] private float m_yInvertion = 1;
+    [SerializeField] private float m_xInvertion = 1;
+
+    private Camera m_cam;
+
+    private float m_xRotation;
+    private float m_yRotation;
+
+    private void Start()
+    {
+        m_cam = GetComponentInChildren<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+            float mouseX = Input.GetAxisRaw("Mouse X");
+            float mouseY = Input.GetAxisRaw("Mouse Y");
+
+            //Set the values of rotations
+            m_yRotation += mouseX * m_mouseSensitivity * m_xInvertion * Time.deltaTime;
+            m_xRotation += mouseY * m_mouseSensitivity * m_yInvertion * Time.deltaTime;
+
+            m_xRotation = Mathf.Clamp(m_xRotation, -80f, 80);
+
+            //Affectation of values
+            m_cam.transform.localRotation = Quaternion.Euler(m_xRotation, 0, 0);
+            transform.rotation = Quaternion.Euler(0, m_yRotation, 0);
+    }
+
+}
