e14b1cdb3403b81c672a33d6e396618cd33587d5
Jb <ostache.o@gmail.com>
2021-04-10 17:47:02 +0200

    "magnétisme"
    Le code est dégueulasse, il fonctionne mal, mais il fonctionne

---
+++ Assets/Script/Player/PickObject.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickObject : MonoBehaviour
+{
+    [SerializeField] private string m_selectableObject = "Selectable";
+
+    [SerializeField] private GameObject m_newParent;
+    [SerializeField] private GameObject m_parent;
+
+    private bool isGrabbing;
+
+    Transform m_selection;
+
+    private void Start()
+    {
+        isGrabbing = false;
+    }
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit) && !isGrabbing)
+        {
+            if (Input.GetButton("Fire2"))
+            {
+                m_selection = hit.transform;
+                m_selection.GetComponent<Rigidbody>().useGravity = false;
+                if (m_selection.CompareTag(m_selectableObject))
+                {
+                    m_selection.parent = Camera.main.transform;
+                    isGrabbing = true;
+                }
+                
+            }
+        }else if (isGrabbing)
+        {
+            if (Input.GetButton("Fire2"))
+            {
+                m_selection.parent = m_parent.transform;
+                m_selection.GetComponent<Rigidbody>().useGravity = true;
+                isGrabbing = false;
+                m_selection = null;
+            }
+        }
+
+
+    }
+
+
+
+}
