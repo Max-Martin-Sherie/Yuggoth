07ae8f8f4116cf593896b98a795239514d5e86e3
BOYER Noëlie <nono_boyer@icloud.com>
2021-05-05 22:08:05 +0200

    Update Timer Tower
    Add :
    • Verification du raycast complet
    • Inversion de l'effet du mecanisme (activation ou desactivation avec un bool)
    Missing :
    • Les cylindres retournent à leur place originale après un certain temps

--- Assets/Scripts/Level Mecanisms/PotoManager.cs
+++ Assets/Scripts/Level Mecanisms/PotoManager.cs
@@ -4,15 +4,24 @@ using System.Collections.Generic;
 
 public class PotoManager : MonoBehaviour
 {
-    //Stock the cylinders gameObjects 
+    //Stock the cylinders gameObjects (children of an empty object) 
     public List<GameObject> m_cylinders = new List<GameObject>();
-    //Stock the original rotations of the cylinders 
+    //Stock the initial rotations of the cylinders 
     public List<Quaternion> m_initialLocalRotations = new List<Quaternion>();
+    //stock the current rotations of the cylinders
+    public List<Quaternion> m_currentLocalRotations = new List<Quaternion>();
+    //Create the interpolation ratio 
+    private float m_interpolationRatio = 0.1f;
+    
 
-    //Vector3.Lerp(Vector3.up, Vector3.forward, interpolationRatio);
 
     private void Start()
     {
+        //Get the cylinders 
+        for (int i = 0; i < 3; i++)
+        {
+            m_cylinders.Add(transform.GetChild (i).gameObject);
+        }
         //Get the original rotations of the cylinders
         for (int i = 0; i < m_cylinders.Count; i++)
         {
@@ -20,4 +29,29 @@ public class PotoManager : MonoBehaviour
         }
         
     }
+
+    private void OnEnable()
+    {
+        CubeCaster.onRaycastComplete += GetToInitialRotation;
+    }
+
+    private void OnDisable()
+    {
+        CubeCaster.onRaycastComplete -= GetToInitialRotation;
+    }
+
+    private void GetToInitialRotation()
+    {
+        for (int i = 0; i < m_cylinders.Count; i++)
+        {
+            m_currentLocalRotations.Add(m_cylinders[i].transform.localRotation);
+        }
+        for (int i = 0; i < m_cylinders.Count; i++)
+        {
+            //Start lerp if the raycast is completed to allow cylinder to return to their original pos
+            Quaternion.Lerp(m_currentLocalRotations[i], m_initialLocalRotations[i], m_interpolationRatio);
+        }
+        
+    }
+    
 }
